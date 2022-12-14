181b631e48454b2b2ac0b0be350327427b35c518
TrueYahir <Yahir.Orozco@fundacion-jala.org>
2022-12-14 15:53:35 -0800

    Mover

--- dino_runner/components/Dinosaur.py
+++ dino_runner/components/Dinosaur.py
@@ -3,19 +3,30 @@ import os
 import random
 
 from pygame.sprite import Sprite
-from utils.constants import RUNNING
+from dino_runner.utils.constants import RUNNING
 
 class Dinosaur(Sprite):
     def __init__(self):
         #Initializing dino constants
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
-        self.dino_rect_x = 80
-        self.dino_rect_y = 310
+        self.dino_rect_x = self.POS_X
+        self.dino_rect_y = self.POS_Y
+        self.step_index = 0
        
     def update(self, userInput):
-        #update of dino
-        pass
+        self.run()
+        #update of steps
+        if self.step_index >= 10:
+            self.step_index = 0
+
+    def run(self):
+        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect_x = self.POS_X
+        self.dino_rect_y = self.POS_Y
+        self.step_index += 1
+
 
 
     def duck():

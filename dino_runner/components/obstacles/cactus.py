f2bbb2ebb31e5d407aba266f671e4eb8a9c2b39b
TrueYahir <Yahir.Orozco@fundacion-jala.org>
2022-12-15 16:21:24 -0800

    class 3 obstacles

---
+++ dino_runner/components/obstacles/cactus.py
@@ -0,0 +1,9 @@
+import random
+from dino_runner.components.obstacles.obstacle import Obstacle
+
+class Cactus(Obstacle):
+
+    def __init__(self, image):
+        self.type = random.radint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 325
